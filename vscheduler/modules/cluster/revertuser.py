8d05c6c894d18e01b979c5ac8c870fee584586cd
Ali Zamani <ali.zamani@pawsey.org.au>
2023-06-30 20:15:44 +0800

    added logout script

---
+++ vscheduler/modules/cluster/revertuser.py
@@ -0,0 +1,15 @@
+# reverts user to general pool at logout
+from vscheduler.lib.config import Credentials as MyCredentials
+from vscheduler.modules.guaca.entity import entity
+from vscheduler.modules.guaca.guacausergroup import guacamole_user_group
+from vscheduler.modules.guaca.update import update
+
+def revert(user):
+    pool_entity = entity(MyCredentials.pool)
+    pool_group = guacamole_user_group(pool_entity[0][0])
+
+    user_entity = entity(user)
+    update(user_entity[0][0], pool_group[0][0])
+
+# if __name__ == "__main__":
+#     revert()
