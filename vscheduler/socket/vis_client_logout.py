ea4814a114a2f2c00a9c038a12e7c8a4425d904a
Ali Zamani <alizamani84@live.com>
2023-07-14 11:03:52 +0000

    added exception for vis clients login/out

--- vscheduler/socket/vis_client_logout.py
+++ vscheduler/socket/vis_client_logout.py
@@ -7,19 +7,21 @@ PORT = 65001
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
+exception = ["admin"]
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    print(f"[CONNECTED] VIS CLIENT TO MGMT SERVER AT {IP}:{PORT}")
+    if user not in exception:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        print(f"[CONNECTED] VIS CLIENT TO MGMT SERVER AT {IP}:{PORT}")
 
-    connected = True
-    while connected:
-        msg = [host, user]
-        client.send((msg[0] + "," + msg[1] + ",logout").encode(FORMAT))
-        msg = client.recv(SIZE)
-        print(f"[MGMT SERVER] sent: {msg}")
-        connected = False
+        connected = True
+        while connected:
+            msg = [host, user]
+            client.send((msg[0] + "," + msg[1] + ",logout").encode(FORMAT))
+            msg = client.recv(SIZE)
+            print(f"[MGMT SERVER] sent: {msg}")
+            connected = False
         
 if __name__ == "__main__":
     main()
