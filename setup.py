e6ba88fa2225bf4255a75d9eb7f9d17e45bb585f
Ali Zamani <alizamani84@live.com>
2023-07-14 04:43:49 +0000

    updated setup file, added .gitignore

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate',
+        'Click==8.1.3','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate==0.9.0', 'rich==13.3.5'
     ],
     entry_points={
         'console_scripts': [
+            'vkill = vscheduler.tools.kill:main',
             'vsync = vscheduler.tools.guaca:main',
             'vquota = vscheduler.tools.quota:main',
             'vmanage = vscheduler.tools.booked:main',
-            # 'vinfo = vscheduler.tools.info:cli',
-            # 'vreport = vscheduler.tools.report:main',
             'valloc = vscheduler.tools.allocate:main',
+            # 'vreport = vscheduler.tools.report:main',
             # 'vcontrol = vscheduler.tools.control:cli',
+            # 'vinfo = vscheduler.tools.info:cli',
         ],
     },
 )
