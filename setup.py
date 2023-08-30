072eb28a06318dfe3db45693359612f037549ae4
Ali Zamani <alizamani84@live.com>
2023-08-30 11:34:51 +0000

    updated setup to implement report module

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click==8.1.3','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate==0.9.0', 'rich==13.3.5'
+        'Click==8.1.3','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate==0.9.0', 'rich==13.3.5', 'jinja2==3.1.2', 'plotly==5.16.1', 'kaleido==0.2.1'
     ],
     entry_points={
         'console_scripts': [
@@ -15,7 +15,7 @@ setup(
             'vquota = vscheduler.tools.quota:main',
             'vmanage = vscheduler.tools.booked:main',
             'valloc = vscheduler.tools.allocate:main',
-            # 'vreport = vscheduler.tools.report:main',
+            'vreport = vscheduler.tools.report:main',
             # 'vcontrol = vscheduler.tools.control:cli',
             # 'vinfo = vscheduler.tools.info:cli',
         ],
