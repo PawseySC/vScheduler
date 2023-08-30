26b8136ac6a7618bf91d3106f675d319ad54ae3a
Ali Zamani <alizamani84@live.com>
2023-08-30 11:19:06 +0000

    added report modules - to be completed

---
+++ vscheduler/general/alert2.py
@@ -0,0 +1,57 @@
+from email.message import EmailMessage
+from email.utils import make_msgid
+import mimetypes, smtplib
+
+def email_with_embeded_image(body):
+    msg = EmailMessage()
+
+    # generic email headers
+    msg['Subject'] = 'Hello there'
+    msg['From'] = 'noreply@pawsey.org.au'
+    msg['To'] = 'ali.zamani@pawsey.org.au'
+
+    # set the plain text body
+    msg.set_content('This is a plain text body.')
+
+    # now create a Content-ID for the image
+    image_cid = make_msgid(domain='xyz.com')
+    # if `domain` argument isn't provided, it will 
+    # use your computer's name
+
+    # set an alternative html body
+    msg.add_alternative(body.format(image_cid=image_cid[1:-1]), subtype='html')
+    # msg.add_alternative("""\
+    # <html>
+    #     <body>
+    #         <p>This is an HTML body.<br>
+    #            It also has an image.
+    #         </p>
+    #         <img src="cid:{image_cid}">
+    #     </body>
+    # </html>
+    # """.format(image_cid=image_cid[1:-1]), subtype='html')
+    # image_cid looks like <long.random.number@xyz.com>
+    # to use it as the img src, we don't need `<` or `>`
+    # so we use [1:-1] to strip them off
+
+
+    # now open the image and attach it to the email
+    with open('/home/ubuntu/visualisation_scheduler/vscheduler/reports/fig1.png', 'rb') as img:
+
+        # know the Content-Type of the image
+        maintype, subtype = mimetypes.guess_type(img.name)[0].split('/')
+
+        # attach it
+        msg.get_payload()[1].add_related(img.read(), 
+                                            maintype=maintype, 
+                                            subtype=subtype, 
+                                            cid=image_cid)
+
+
+    # the message is ready now
+    # you can write it to a file
+    # or send it using smtplib
+
+    sender = smtplib.SMTP('mail-server.pawsey.org.au')
+    sender.send_message(msg)            # python 3
+    sender.quit()
