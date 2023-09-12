28e79bee7f367eaafb9b5e477b3d81d38945aaed
Ali Zamani <alizamani84@live.com>
2023-09-12 09:11:24 +0800

    insert module for ldap_mysql -> to be merged with current functions

--- vscheduler/modules/guaca/revert_user.py
+++ vscheduler/modules/guaca/revert_user.py
@@ -4,6 +4,7 @@ from vscheduler.lib.config import Credentials as MyCredentials
 from vscheduler.modules.guaca.entity import entity
 from vscheduler.modules.guaca.guaca_user_group import guacamole_user_group
 from vscheduler.modules.guaca.update import update
+from vscheduler.modules.guaca.insert import insert
 from vscheduler.general.alert import mailFunction
 
 pool_records = Capture_log("pool", __file__)
@@ -25,5 +26,18 @@ def revert(user):
     logger.info (f"user_entity: {user_entity}")
     update(user_entity[0][0], pool_group[0][0])
 
-# if __name__ == "__main__":
-#     revert()
+def insert_new_to_pool(user):
+    pool_entity = entity(MyCredentials.pool)
+    logger.info (f"pool_entity: {pool_entity}")
+    if pool_entity is not None:
+        pool_group = guacamole_user_group(pool_entity[0][0])
+    else:
+        mailFunction("NoneType error",f"NoneType object is not subscriptable\nvscheduler > modules > cluster > revertuser > revert (line 16)\npool_group = guacamole_user_group({pool_entity}) = {pool_group}", "", "")
+        logger.critical (f"NoneType object is not subscriptable\nvscheduler > modules > cluster > revertuser > revert (line 16)\npool_group = guacamole_user_group({pool_entity}) = {pool_group}")
+        pass
+
+    user_entity = entity(user)
+    logger.info (f"pool_entity: {pool_entity}")
+    logger.info (f"pool_group: {pool_group}")
+    logger.info (f"user_entity: {user_entity}")
+    insert(user_entity[0][0], pool_group[0][0])
