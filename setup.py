0ed3ed38d1a39967594d74e473844cb87b347b25
Ali Zamani <alizamani84@live.com>
2023-10-19 10:24:24 +0800

    added packages to setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click==8.1.3','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate==0.9.0', 'rich==13.3.5', 'jinja2==3.1.2', 'plotly==5.16.1', 'kaleido==0.2.1'
+        'Click==8.1.3','paramiko==3.0.0', 'PyMySQL==1.0.2', 'numpy==1.24.3', 'tabulate==0.9.0', 'rich==13.3.5', 'jinja2==3.1.2', 'plotly==5.16.1', 'kaleido==0.2.1', 'calmap==0.0.11', 'calplot==0.1.7.5'
     ],
     entry_points={
         'console_scripts': [
