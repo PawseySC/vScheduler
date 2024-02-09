a46d3a674b14856975ac1c72f03f4d29be75e752
Ali Zamani <alizamani84@live.com>
2024-02-09 13:31:04 +0000

    optimised socket files

--- vscheduler/socket/mgmt_client.py
+++ vscheduler/socket/mgmt_client.py
@@ -1,41 +1,49 @@
 import socket, time
 from vscheduler.log.log import Capture_log
-from vscheduler.lib.config import Credentials as MyCredentials
+from vscheduler.general.initiate import PrintCondition as MyPrintCondition
 from vscheduler.general.alert import mailFunction
 
+port = 65002
+
+socket_records = Capture_log("socket", __file__)
+logger = socket_records.log_agent()
+
+
 def client_program(domains):
-    # hosts = ["192.168.2.66", "192.168.2.144"]
-    print ("host", domains)
+    print (f"domains: {domains}") if MyPrintCondition.fprint else 0
+    logger.info (f"domains: {domains}")
     hosts =[]
 
+    # fetch domain IPs
     for domain in domains:
         # addr = socket.getaddrinfo (domain, 0,0,0,0)
         # for result in addr:
         #     hosts.append(result[-1][0])
         #     hosts = list(set(hosts))
         hosts = list({addr[-1][0] for addr in socket.getaddrinfo (domain, 0, 0, 0, 0)})
+    print (f"hosts IPs: {hosts}") if MyPrintCondition.fprint else 0
+    logger.info (f"hosts IPs: {hosts}")
 
-    port = 65002
     all_data = {}
-
-    socket_records = Capture_log("socket", __file__)
-    logger = socket_records.log_agent()
-
     for host in hosts:
         client_socket = socket.socket()                 # instantiate
         try:
             client_socket.connect((host, port))         # connect to the server
         except socket.error as e:
+            print (f"Caught exception socket.error: {e} {host} {port}") if MyPrintCondition.fprint else 0
             logger.critical (f"Caught exception socket.error: {e} {host} {port}")
             mailFunction("socket error","error connecting vis node socket server\n" + e + " " + host + " " + port, "", "")
             continue
 
         message = "Requesting data from " + str(host)
+        print (message) if MyPrintCondition.fprint else 0
+        logger.info (message)
 
         while True:
             client_socket.send(message.encode())        # send message
             data = client_socket.recv(1024).decode()    # receive response
 
+            print (f"Received from {host}: {data}") if MyPrintCondition.fprint else 0
             logger.info (f"Received from {host}: {data}")
             all_data[host] = data.split(",")
             time.sleep(0.1)
@@ -44,7 +52,6 @@ def client_program(domains):
         
         client_socket.close()  # close the connection
         
-
     return all_data
 
 # if __name__ == '__main__':
