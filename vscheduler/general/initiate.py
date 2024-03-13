43de84ccc2a14679ec800a9efae5f6c8735110b6
Ali Zamani <alizamani84@live.com>
2024-03-13 21:57:02 +0800

    redesigned cli structure for vset

--- vscheduler/general/initiate.py
+++ vscheduler/general/initiate.py
@@ -13,29 +13,30 @@ class Initiation:
     
     for arg in range (1, arg_num-1):
         if sys.argv[arg] == '-u':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--add' and sys.argv[arg+1] != '--remove' and sys.argv[arg+1] != '--status' and sys.argv[arg+1] != '--email' and sys.argv[arg+1] != '--activate' and sys.argv[arg+1] != '--deactivate':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
                 user = sys.argv[arg+1]
         elif sys.argv[arg] == '-n':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--add' and sys.argv[arg+1] != '--remove' and sys.argv[arg+1] != '--status' and sys.argv[arg+1] != '--email' and sys.argv[arg+1] != '--activate' and sys.argv[arg+1] != '--deactivate':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
                 node = sys.argv[arg+1]
         elif sys.argv[arg] == '-d':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--add' and sys.argv[arg+1] != '--remove' and sys.argv[arg+1] != '--status' and sys.argv[arg+1] != '--email' and sys.argv[arg+1] != '--activate' and sys.argv[arg+1] != '--deactivate':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
                 start = sys.argv[arg+1]
-                if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v' and sys.argv[arg+2] != '--add' and sys.argv[arg+2] != '--remove' and sys.argv[arg+2] != '--status' and sys.argv[arg+1] != '--email' and sys.argv[arg+1] != '--activate' and sys.argv[arg+1] != '--deactivate':
+                if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v' and sys.argv[arg+2] != '--status':
                     end = sys.argv[arg+2]
-        elif sys.argv[arg] == '--add':
-            status = 'add'
-        elif sys.argv[arg] == '--remove':
-            status = 'remove'
-        elif sys.argv[arg] == '--status':
-            status = 'status'
-        elif sys.argv[arg] == '--email':
-            email = True
-        elif sys.argv[arg] == '--activate':
-            status = 'activate'
-        elif sys.argv[arg] == '--deactivate':
-            status = 'deactivate'
-            
+        elif sys.argv[arg] == '--status' and arg+1 <= arg_num-1:
+            if sys.argv[arg+1] == 'up':
+                status = 'up'                       # up means fresh node in production with no one logged in = idle
+            elif sys.argv[arg+1] == 'down':
+                status = 'down'
+            elif sys.argv[arg+1] == 'maintenance':
+                status = 'maint'
+            elif sys.argv[arg+1] == 'allocated':    # any node with logged in user
+                status = 'allocated'
+            elif sys.argv[arg+1] == 'idle':         # nodes from bookable partition which are not booked and with no logged in user OR nodes not allocated to any user in general pool (meaning no user is logged in)
+                status = 'idle'
+            elif sys.argv[arg+1] == 'reserved':     # booked (only for bookable partition)
+                status = 'reserved'
+        
 
     if sys.argv[1:].count('-u') > 1 or sys.argv[1:].count('-n') > 1 or sys.argv[1:].count('-d') > 1 or sys.argv[1:].count('--add') > 1 or sys.argv[1:].count('--remove') > 1 or sys.argv[1:].count('--status') > 1:
         print ("error in arguments, please see help")
