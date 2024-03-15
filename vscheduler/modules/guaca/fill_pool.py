f26b24404fc932290f1c0cef732b5cfac9bfc390
Ali Zamani <alizamani84@live.com>
2024-03-15 21:24:31 +0800

    pool fill up - to be tested and debugged

--- vscheduler/modules/guaca/fill_pool.py
+++ vscheduler/modules/guaca/fill_pool.py
@@ -9,8 +9,8 @@ pool_records = Capture_log("pool", __file__)
 logger_win = pool_records.log_agent("windows")
 logger_unix = pool_records.log_agent("linux")
 
-def fillup(node):
-    logger_win.info (f"Current node in the pool is {node}") if MyCredentials.windows_node_name in node else logger_unix.info (f"Current node in the pool is {node}")
+
+def toss(node):
     if MyCredentials.windows_node_name in node:
         current_number = int(node.replace(MyCredentials.windows_node_name, ""))
         logger_win.info (f"current_number+1: {current_number+1}") if MyCredentials.windows_node_name in node else logger_unix.info (f"current_number+1: {current_number+1}")
@@ -20,9 +20,8 @@ def fillup(node):
         else:
             current_number = MyCredentials.windows_general_range[0]
         new_node = MyCredentials.windows_node_name + "0" + str(current_number) if current_number <= 9 else MyCredentials.windows_node_name + str(current_number)
-        pool_entity = entity(MyCredentials.windows_pool)
 
-    if MyCredentials.linux_node_name in node:
+    elif MyCredentials.linux_node_name in node:
         current_number = int(node.replace(MyCredentials.linux_node_name, ""))
         logger_win.info (f"current_number+1: {current_number+1}") if MyCredentials.windows_node_name in node else logger_unix.info (f"current_number+1: {current_number+1}")
         if current_number+1 in range(MyCredentials.linux_general_range[0], MyCredentials.linux_general_range[1]+1):
@@ -31,7 +30,18 @@ def fillup(node):
         else:
             current_number = MyCredentials.linux_general_range[0]
         new_node = MyCredentials.linux_node_name + "0" + str(current_number) if current_number <= 9 else MyCredentials.linux_node_name + str(current_number)
-        pool_entity = entity(MyCredentials.linux_pool)
+        
+    return new_node
+
+
+def fillup(node, hosts):
+    logger_win.info (f"Current node in the pool is {node}") if MyCredentials.windows_node_name in node else logger_unix.info (f"Current node in the pool is {node}")
+    new_node = toss (node)
+    if len (hosts) > 0:
+        while new_node not in hosts:
+            new_node = toss (new_node)
+    
+    pool_entity = entity(MyCredentials.windows_pool) if MyCredentials.windows_node_name in node else entity(MyCredentials.linux_pool)
 
     logger_win.info (f"New node in the pool is {new_node}") if MyCredentials.windows_node_name in node else logger_unix.info (f"New node in the pool is {new_node}")
     new_node_entity = entity(new_node)
