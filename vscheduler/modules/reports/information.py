e28382d02bc3a0f23c4518048efdc154d8fa8905
Ali Zamani <alizamani84@live.com>
2024-03-18 21:54:33 +0800

    vinfo working

--- vscheduler/modules/reports/information.py
+++ vscheduler/modules/reports/information.py
@@ -7,12 +7,18 @@ from vscheduler.general.timer import Brackets as MyBrackets
 from tabulate import tabulate
 import pandas as pd
 import numpy as np
+import pandas
 
 my_connection = MyDatabase.connect_report_db()
 
 records = Capture_log("status", __file__)
 logger = records.log_agent("info")
 
+linux_general_hosts = [MyCredentials.linux_node_name + "0" + str(i) if i < 10 else MyCredentials.linux_node_name + str(i) for i in range(MyCredentials.linux_general_range[0], MyCredentials.linux_general_range[1]+1)]
+linux_booking_hosts = [MyCredentials.linux_node_name + "0" + str(i) if i < 10 else MyCredentials.linux_node_name + str(i) for i in range(MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1)]
+windows_general_hosts = [MyCredentials.windows_node_name + "0" + str(i) if i < 10 else MyCredentials.windows_node_name + str(i) for i in range(MyCredentials.windows_general_range[0], MyCredentials.windows_general_range[1]+1)]
+windows_booking_hosts = [MyCredentials.windows_node_name + "0" + str(i) if i < 10 else MyCredentials.windows_node_name + str(i) for i in range(MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1)]
+        
 
 def print_info():
     try:
@@ -20,43 +26,57 @@ def print_info():
         with my_connection.cursor() as my_cursor:
             my_cursor.execute(query)
             query_results = my_cursor.fetchall()
-        print ("\n", tabulate(query_results, headers=['node', 'status', 'pool'])) if MyPrintCondition.fprint and query_results else 0
-        logger.info ("\n" + tabulate(query_results, headers=['node', 'status', 'pool']))
+        print ("\n", tabulate(query_results, headers=['node', 'status', 'pool'], tablefmt='psql')) if MyPrintCondition.fprint and query_results else 0
+        logger.info ("\n" + tabulate(query_results, headers=['node', 'status', 'pool'], tablefmt='psql'))
         
-        linux_general_hosts = [MyCredentials.linux_node_name + "0" + str(i) if i < 10 else MyCredentials.linux_node_name + str(i) for i in range(MyCredentials.linux_general_range[0], MyCredentials.linux_general_range[1]+1)]
-        linux_booking_hosts = [MyCredentials.linux_node_name + "0" + str(i) if i < 10 else MyCredentials.linux_node_name + str(i) for i in range(MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1)]
-        windows_general_hosts = [MyCredentials.windows_node_name + "0" + str(i) if i < 10 else MyCredentials.windows_node_name + str(i) for i in range(MyCredentials.windows_general_range[0], MyCredentials.windows_general_range[1]+1)]
-        windows_booking_hosts = [MyCredentials.windows_node_name + "0" + str(i) if i < 10 else MyCredentials.windows_node_name + str(i) for i in range(MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1)]
-
+        a = np.array(query_results)
+        print (f'query_results[np.where(np.array(query_results) == node)[0][1]]: {np.where(a == "setonix-vis05")[0][0]}')
+        print (f"linux_general_hosts: {linux_general_hosts}")
         sentence = []
         for node in linux_general_hosts:
             if node in np.array(query_results)[:,0]:
                 print (f"{node} currently in db")
-                sentence.insert(len(sentence), [node , np.array(query_results)[:,0], np.array(query_results)[:,0]])
+                sentence.insert(len(sentence), [node , query_results[np.where(a == node)[0][0]][1], query_results[np.where(a == node)[0][0]][2]])
             else:
-                sentence.insert(len(sentence), [node, "idle", pool])
+                sentence.insert(len(sentence), [node, "idle", "general"])
         for node in linux_booking_hosts:
             if node in np.array(query_results)[:,0]:
                 print (f"{node} currently in db")
-                sentence.insert(len(sentence), [node , np.array(query_results)[:,0], np.array(query_results)[:,0]])
+                sentence.insert(len(sentence), [node , query_results[np.where(a == node)[0][0]][1], query_results[np.where(a == node)[0][0]][2]])
             else:
-                sentence.insert(len(sentence), [node, "idle", pool])
+                sentence.insert(len(sentence), [node, "idle", "booking"])
         for node in windows_general_hosts:
             if node in np.array(query_results)[:,0]:
                 print (f"{node} currently in db")
-                sentence.insert(len(sentence), [node , np.array(query_results)[:,0], np.array(query_results)[:,0]])
+                sentence.insert(len(sentence), [node , query_results[np.where(a == node)[0][0]][1], query_results[np.where(a == node)[0][0]][2]])
             else:
-                sentence.insert(len(sentence), [node, "idle", pool])
+                sentence.insert(len(sentence), [node, "idle", "general"])
         for node in windows_booking_hosts:
             if node in np.array(query_results)[:,0]:
                 print (f"{node} currently in db")
-                sentence.insert(len(sentence), [node , np.array(query_results)[:,0], np.array(query_results)[:,0]])
+                sentence.insert(len(sentence), [node , query_results[np.where(a == node)[0][0]][1], query_results[np.where(a == node)[0][0]][2]])
             else:
-                sentence.insert(len(sentence), [node, "idle", pool])
-        
-        print ("\n", tabulate(sentence, headers=['node', 'status', 'pool'])) if MyPrintCondition.fprint and query_results else 0
-        logger.info ("\n" + tabulate(sentence, headers=['node', 'status', 'pool']))
+                sentence.insert(len(sentence), [node, "idle", "booking"])
+        print (f"sentence: {sentence}")
+        print ("\n", tabulate(sentence, headers=['node', 'status', 'pool'], tablefmt='psql')) if MyPrintCondition.fprint and query_results else 0
+        logger.info ("\n" + tabulate(sentence, headers=['node', 'status', 'pool'], tablefmt='psql'))
                     
     except my_connection.Error as e:
         print (f"status query hit error\n{e}") if MyPrintCondition.fprint else 0
         logger.error (f"status query hit error\n{e}")
+        
+        
+def session (node):
+    cluster = MyCredentials.report_linux_table if MyCredentials.linux_node_name in node else MyCredentials.report_windows_table
+    query = f"SELECT node, user, pool, start, end from {cluster} WHERE node = '{node}' AND start = end"
+    with my_connection.cursor() as my_cursor:
+        my_cursor.execute(query)
+        query_results = my_cursor.fetchall()
+    # print (query, query_results)
+    # a = dict(query_results)
+    if query_results and (node in linux_general_hosts or node in windows_general_hosts):
+        print (f"\nLogged-in Users in < {node} >, {cluster} cluster\n", tabulate(query_results, headers=['node', 'user', 'pool', 'start', 'end'], tablefmt='psql')) if MyPrintCondition.fprint and query_results else 0
+        logger.info (f"\nLogged-in Users in < {node} >, {cluster} cluster\n" + tabulate(query_results, headers=['node', 'user', 'pool', 'start', 'end'], tablefmt='psql'))
+    else:
+        print (f"No one's logged in to < {node} >")
+        logger.info (f"No one's logged in to < {node} >")
--- vscheduler/tools/info.py
+++ vscheduler/tools/info.py
@@ -6,13 +6,64 @@ from vscheduler.lib.config import Credentials as MyCredentials
 from vscheduler.general.initiate import Initiation as initiate
 from vscheduler.general.initiate import PrintCondition as MyPrintCondition
 from vscheduler.modules.reports.information import print_info
+from vscheduler.modules.reports.information import session
 
 records = Capture_log("info", __file__)
-logger_win = records.log_agent("info")
+logger = records.log_agent("info")
 
 
+# Process class
+class Process(multiprocessing.Process):
+    def __init__(self, id, node):
+        super(Process, self).__init__()
+        self.id = id
+        self.hostname = node
+    
+    def run(self):
+        session (self.hostname)
+        
 def main():
-    print_info()
+    if initiate.session:
+        print (initiate.session)
+        if not initiate.node:
+            if MyCredentials.windows_booking or MyCredentials.windows_general:
+                if MyCredentials.windows_booking:
+                    for i in range (MyCredentials.windows_booking_range[0], MyCredentials.windows_booking_range[1]+1):
+                        node = MyCredentials.windows_node_name + '0' + str(i) if i <= 9 else MyCredentials.windows_node_name + str(i)
+                        p = Process(i, node)
+                        p.start()       # Create a new process and invoke the Process.run() method
+                        p.join()        # Process.join() to wait for task completion
+                if MyCredentials.windows_general:
+                    for i in range (MyCredentials.windows_general_range[0], MyCredentials.windows_general_range[1]+1):
+                        node = MyCredentials.windows_node_name + '0' + str(i) if i <= 9 else MyCredentials.windows_node_name + str(i)
+                        p = Process(i, node)
+                        p.start()       # Create a new process and invoke the Process.run() method
+                        p.join()        # Process.join() to wait for task completion
+            else:
+                print ("There is no Windows partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
+                logger.info ("There is no Windows partition; To enable it edit vscheduler confilg")
+            if MyCredentials.linux_booking or MyCredentials.linux_general:
+                if MyCredentials.linux_booking:
+                    for i in range (MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1):
+                        node = MyCredentials.linux_node_name + '0' + str(i) if i <= 9 else MyCredentials.linux_node_name + str(i)
+                        p = Process(i, node)
+                        p.start()       # Create a new process and invoke the Process.run() method
+                        p.join()        # Process.join() to wait for task completion
+                if MyCredentials.linux_general:
+                    for i in range (MyCredentials.linux_general_range[0], MyCredentials.linux_general_range[1]+1):
+                        node = MyCredentials.linux_node_name + '0' + str(i) if i <= 9 else MyCredentials.linux_node_name + str(i)
+                        p = Process(i, node)
+                        p.start()       # Create a new process and invoke the Process.run() method
+                        p.join()        # Process.join() to wait for task completion
+            else:
+                print ("There is no Linux partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
+                logger.info ("There is no Linux partition; To enable it edit vscheduler confilg")    
+        elif initiate.node:
+            p = Process("", initiate.node)
+            p.start()       # Create a new process and invoke the Process.run() method
+            p.join()        # Process.join() to wait for task completion
+    else:
+        print_info()
 
 if __name__ == '__main__':
     main()
