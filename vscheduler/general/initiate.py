3b81758b937b7ef6dc412d0e3830b22fc7c760a8
Ali Zamani <ali.zamani@pawsey.org.au>
2024-09-18 11:34:39 +0800

    added vadmin mogule

--- vscheduler/general/initiate.py
+++ vscheduler/general/initiate.py
@@ -5,39 +5,56 @@ from vscheduler.general.help import Help
 arg_num = len(sys.argv)
 
 class PrintCondition():
-    fprint = True if '-v' in sys.argv[1:] or '--verbose' in sys.argv[1:] else False
+    fprint = True if '-v' in sys.argv[1:] else False
 
 class Initiation:
     node = user = start = end = status = ''
-    email = session = False
+    email = session = list = activate = deactivate = False
     
     for arg in range (1, arg_num-1):
         if sys.argv[arg] == '-u':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v':
                 user = sys.argv[arg+1]
+            else:
+                print ("error in arguments, please see help")
+                sys.exit()
         elif sys.argv[arg] == '-n':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v':
                 node = sys.argv[arg+1]
+            else:
+                print ("error in arguments, please see help")
+                sys.exit()
         elif sys.argv[arg] == '-d':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '--status':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v':
                 start = sys.argv[arg+1]
-                if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v' and sys.argv[arg+2] != '--status':
+                if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v':
                     end = sys.argv[arg+2]
-        elif sys.argv[arg] == '--status' and arg+1 <= arg_num-1:
-            if sys.argv[arg+1] == 'up':
-                status = 'up'                       # up means fresh node in production with no one logged in = idle
-            elif sys.argv[arg+1] == 'down':
-                status = 'down'
-            elif sys.argv[arg+1] == 'maint':
-                status = 'maint'
-            elif sys.argv[arg+1] == 'allocated':    # any node with logged in user
+                else:
+                    print ("error in arguments, please see help")
+                    sys.exit()
+            else:
+                print ("error in arguments, please see help")
+                sys.exit()
+        elif sys.argv[arg+1] == 'up':
+            status = 'up'                       # up means fresh node in production with no one logged in = idle
+        elif sys.argv[arg+1] == 'down':
+            status = 'down'
+        elif sys.argv[arg+1] == 'maint':
+            status = 'maint'
+        elif sys.argv[arg+1] == 'allocated':    # any node with logged in user
                 status = 'allocated'
-            # elif sys.argv[arg+1] == 'idle':         # nodes from bookable partition which are not booked and with no logged in user OR nodes not allocated to any user in general pool (meaning no user is logged in)
-            #     status = 'idle'
-            elif sys.argv[arg+1] == 'reserved':     # booked (only for bookable partition)
-                status = 'reserved'
-        elif sys.argv[arg] == '--session':
-            session = True
+        # elif sys.argv[arg+1] == 'idle':         # nodes from bookable partition which are not booked and with no logged in user OR nodes not allocated to any user in general pool (meaning no user is logged in)
+        #     status = 'idle'
+        elif sys.argv[arg+1] == 'reserved':     # booked (only for bookable partition)
+            status = 'reserved'
+        elif sys.argv[arg] == 'status':
+            status = True
+        elif sys.argv[arg] == 'list':
+            list = True
+        elif sys.argv[arg] == 'activate':
+            activate = True
+        elif sys.argv[arg] == 'deactivate':
+            deactivate = True
         
 
     if sys.argv[1:].count('-u') > 1 or sys.argv[1:].count('-n') > 1 or sys.argv[1:].count('-d') > 1 or sys.argv[1:].count('--add') > 1 or sys.argv[1:].count('--remove') > 1 or sys.argv[1:].count('--status') > 1:
---
+++ vscheduler/tools/admin.py
@@ -0,0 +1,66 @@
+# allocates connection link to specific node of general pool in user's guacamole dashboard
+import multiprocessing
+from vscheduler.log.log import Capture_log
+from vscheduler.general.initiate import Initiation as initiate
+from vscheduler.general.initiate import PrintCondition as MyPrintCondition
+from vscheduler.modules.reports.administrator import admin_update, admin_list
+
+
+records = Capture_log("admin", __file__)
+logger_admin = records.log_agent("admin")
+
+
+# Process class
+class Process(multiprocessing.Process):
+    def __init__(self, username, mode):
+        super(Process, self).__init__()
+        self.username = username
+        self.mode = mode
+    
+    def run(self):
+        # time.sleep(1)
+        # print (f"user: {self.username}") if MyPrintCondition.fprint else 0
+        if self.username == "list" or self.mode == "status":
+            admin_list (self.username)
+        elif self.mode == "activate" or self.mode == "deactivate":
+            admin_update (self.username, self.mode)
+
+def main():
+    if not initiate.node:
+        if initiate.list:
+            if initiate.user:
+                print ("no username is needed when listing admins\nplease see help")
+            else:
+                p = Process("", "list")
+                p.start()
+                p.join()
+        elif initiate.status:
+            if not initiate.user:
+                print ("username is needed when querying admin status\nplease see help")
+            else:
+                p = Process(initiate.user, "status")
+                p.start()
+                p.join()
+        elif initiate.activate:
+            if not initiate.user:
+                print ("vadmin activate requires username\nplease see help")
+            else:
+                p = Process(initiate.user, "activate")
+                p.start()
+                p.join()
+                # [(Process(users, "activate"), Process(users, "activate").start(), Process(users, "activate").join()) for users in initiate.user]
+        elif initiate.deactivate:
+            if not initiate.user:
+                print ("vadmin deactivate requires username\nplease see help")
+            else:
+                p = Process(initiate.user, "deactivate")
+                p.start()
+                p.join()
+                # [(Process(users, "deactivate"), Process(users, "deactivate").start(), Process(users, "deactivate").join()) for users in initiate.user]
+    else:
+        print ("vadmin doesn't require node\nplease see help")
+        
+
+
+if __name__ == '__main__':
+    main()
