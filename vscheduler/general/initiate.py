337b6e3a0f00d8a00ce7bfceed6f2377d6e0c518
Ali Zamani <ali.zamani@pawsey.org.au>
2024-09-18 21:41:23 +0800

    vadmin converted to vexcept Following this, comcept of admin is not in effect anymore, in a sense anyone's activities regardless of privilige will be recorded and if it's a general pool they login/out, pool member will be changed. Also, wall time will be applied based on being standard for 8 hrs or non-standard user for any number. Historical search was added to vexcept list & status.

--- vscheduler/general/initiate.py
+++ vscheduler/general/initiate.py
@@ -1,6 +1,7 @@
 # distinguishes commands arguments
 import sys
 from vscheduler.general.help import Help
+from vscheduler.lib.config import Credentials as MyCredentials
 
 arg_num = len(sys.argv)
 
@@ -8,30 +9,38 @@ class PrintCondition():
     fprint = True if '-v' in sys.argv[1:] else False
 
 class Initiation:
-    node = user = start = end = status = ''
-    email = session = list = activate = deactivate = stat = False
+    node = user = start = end = status = mode = time = ''
+    email = session = list = activate = deactivate = False
     
     for arg in range (1, arg_num-1):
         if sys.argv[arg] == '-u':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '-t':
                 user = sys.argv[arg+1]
             else:
                 print ("error in arguments, please see help")
                 sys.exit()
         elif sys.argv[arg] == '-n':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '-t':
                 node = sys.argv[arg+1]
             else:
                 print ("error in arguments, please see help")
                 sys.exit()
+        elif sys.argv[arg] == '-t':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-d' and sys.argv[arg+1] != '-v':
+                time = sys.argv[arg+1]
+            else:
+                print ("error in arguments, please see help")
+                sys.exit()
         elif sys.argv[arg] == '-d':
-            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v':
+            if arg+1 <= arg_num-1 and sys.argv[arg+1] != '-u' and sys.argv[arg+1] != '-n' and sys.argv[arg+1] != '-v' and sys.argv[arg+1] != '-t':
                 start = sys.argv[arg+1]
-                if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v':
-                    end = sys.argv[arg+2]
-                else:
-                    print ("error in arguments, please see help")
-                    sys.exit()
+                if arg+2 <= arg_num-1:
+                    # if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v' and sys.argv[arg+1] != '-t':
+                    if sys.argv[arg+2] and sys.argv[arg+2] != '-u' and sys.argv[arg+2] != '-n' and sys.argv[arg+2] != '-v' and sys.argv[arg+2] != '-t':
+                        end = sys.argv[arg+2]
+                    else:
+                        print ("error in arguments, please see help")
+                        sys.exit()
             else:
                 print ("error in arguments, please see help")
                 sys.exit()
@@ -41,6 +50,8 @@ class Initiation:
             status = 'down'
         elif sys.argv[arg+1] == 'maint':
             status = 'maint'
+        elif sys.argv[arg+1] == 'dev':
+            status = 'dev'                      # dev nodes communicate with dev-vscheduler
         elif sys.argv[arg+1] == 'allocated':    # any node with logged in user
             status = 'allocated'
         # elif sys.argv[arg+1] == 'idle':         # nodes from bookable partition which are not booked and with no logged in user OR nodes not allocated to any user in general pool (meaning no user is logged in)
@@ -48,13 +59,13 @@ class Initiation:
         elif sys.argv[arg+1] == 'reserved':     # booked (only for bookable partition)
             status = 'reserved'
         elif sys.argv[arg] == 'status':
-            stat = True
+            mode = 'status'
         elif sys.argv[arg] == 'list':
-            list = True
+            mode = 'list'
         elif sys.argv[arg] == 'activate':
-            activate = True
+            mode = 'activate'
         elif sys.argv[arg] == 'deactivate':
-            deactivate = True
+            mode = 'deactivate'
         
 
     if sys.argv[1:].count('-u') > 1 or sys.argv[1:].count('-n') > 1 or sys.argv[1:].count('-d') > 1 or sys.argv[1:].count('--add') > 1 or sys.argv[1:].count('--remove') > 1 or sys.argv[1:].count('--status') > 1:
---
+++ vscheduler/tools/except.py
@@ -0,0 +1,69 @@
+# allocates connection link to specific node of general pool in user's guacamole dashboard
+import multiprocessing
+from vscheduler.log.log import Capture_log
+from vscheduler.general.initiate import Initiation as initiate
+from vscheduler.general.initiate import PrintCondition as MyPrintCondition
+from vscheduler.modules.reports.exception import exception_update, exception_list
+
+
+records = Capture_log("exception", __file__)
+logger_exception = records.log_agent("exception")
+
+
+# Process class
+class Process(multiprocessing.Process):
+    def __init__(self, username, mode, start, end, time):
+        super(Process, self).__init__()
+        self.username = username
+        self.mode = mode
+        self.start_date = start
+        self.end_date = end
+        self.time = time
+    
+    def run(self):
+        # time.sleep(1)
+        # print (f"user: {self.username}") if MyPrintCondition.fprint else 0
+        if self.mode == "list" or self.mode == "status":
+            exception_list (self.username, self.start_date, self.end_date)
+        elif self.mode == "activate" or self.mode == "deactivate":
+            exception_update (self.username, self.mode, self.time)
+
+def main():
+    if not initiate.node:
+        if initiate.mode == 'list':
+            if initiate.user:
+                print ("no username is needed when listing exceptions\nplease see help")
+            else:
+                p = Process("", "list", initiate.start, initiate.end, "")
+                p.start()
+                p.join()
+        elif initiate.mode == 'status':
+            if not initiate.user:
+                print ("username is needed when querying exception status\nplease see help")
+            else:
+                p = Process(initiate.user, "status", initiate.start, initiate.end, "")
+                p.start()
+                p.join()
+        elif initiate.mode == 'activate':
+            if not initiate.user or not initiate.time:
+                print ("vexcept activate requires username and time\nplease see help")
+            else:
+                p = Process(initiate.user, "activate", initiate.time)
+                p.start()
+                p.join()
+                # [(Process(users, "activate"), Process(users, "activate").start(), Process(users, "activate").join()) for users in initiate.user]
+        elif initiate.mode == 'deactivate':
+            if not initiate.user:
+                print ("vexcept deactivate requires username\nplease see help")
+            else:
+                p = Process(initiate.user, "deactivate", "")
+                p.start()
+                p.join()
+                # [(Process(users, "deactivate"), Process(users, "deactivate").start(), Process(users, "deactivate").join()) for users in initiate.user]
+    else:
+        print ("vexcept doesn't require node\nplease see help")
+        
+
+
+if __name__ == '__main__':
+    main()
