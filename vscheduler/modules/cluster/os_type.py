0c9a3e8859e5373315a15cfc60e970a97f7e5f19
Ali Zamani <ali.zamani@pawsey.org.au>
2024-10-10 16:09:28 +0800

    os discovery by python instead of os command

--- vscheduler/modules/cluster/os_type.py
+++ vscheduler/modules/cluster/os_type.py
@@ -11,32 +11,44 @@ logger_unix = records.log_agent("linux")
 def find_os(node):
     my_connection = MyNode.connect_node(node)
 
+    if MyCredentials.windows_node_name in node:
+        command = 'python -c "import platform; print (platform.uname()[0])"'
+    else:
+        command = 'python3 -c "import platform; print (platform.uname()[0])"'
+        
     try:
         operating_system = ""
         
-        stdin , stdout, stderr = my_connection.exec_command("ver")
+        # stdin , stdout, stderr = my_connection.exec_command("ver")
+        # if stderr:
+        #     print (f"Errors (Windows): {stderr.read()}") if MyPrintCondition.fprint else 0
+        #     logger.error (f"Errors (Windows): {stderr.read()}")
+        stdin , stdout, stderr = my_connection.exec_command(command)
         if stderr:
-            print (f"Errors (Windows): {stderr.read()}") if MyPrintCondition.fprint else 0
-            logger_win.error (f"Errors (Windows): {stderr.read()}") if MyCredentials.windows_node_name in node else logger_unix.error (f"Errors (Windows): {stderr.read()}")
+            print (f"Errors: {stderr.read()}") if MyPrintCondition.fprint else 0
+            logger_win.error (f"Errors: {stderr.read()}")
+            logger_unix.error (f"Errors: {stderr.read()}")
         for line in stdout:
             print (line.strip('\n')) if MyPrintCondition.fprint else 0
-            logger_win.info (line.strip('\n')) if MyCredentials.windows_node_name in node else logger_unix.info (line.strip('\n'))
+            logger_win.info (line.strip('\n'))
+            logger_unix.info (line.strip('\n'))
             if "Windows" in line.split():
                 operating_system = "Windows"
             else:
                 continue
         
-        stdin , stdout, stderr = my_connection.exec_command("uname")
-        if stderr:
-            print (f"Errors (Linux): {stderr.read()}") if MyPrintCondition.fprint else 0
-            logger_win.error (f"Errors (Linux): {stderr.read()}") if MyCredentials.windows_node_name in node else logger_unix.error (f"Errors (Linux): {stderr.read()}")
-        for line in stdout:
-            print (line.strip('\n')) if MyPrintCondition.fprint else 0
-            logger_win.info (line.strip('\n')) if MyCredentials.windows_node_name in node else logger_unix.info (line.strip('\n'))
-            if "Linux" in line.split():
-                operating_system = "Linux"
-            else:
-                continue
+        
+        # stdin , stdout, stderr = my_connection.exec_command("uname")
+        # if stderr:
+        #     print (f"Errors (Linux): {stderr.read()}") if MyPrintCondition.fprint else 0
+        #     logger.error (f"Errors (Linux): {stderr.read()}")
+        # for line in stdout:
+        #     print (line.strip('\n')) if MyPrintCondition.fprint else 0
+        #     logger.info (line.strip('\n'))
+        #     if "Linux" in line.split():
+        #         operating_system = "Linux"
+        #     else:
+        #         continue
         my_connection.close()
         return operating_system
 
