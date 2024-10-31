8fe58a14a694de39b5571ee3d17ec37a7dc09cf9
Ali Zamani <ali.zamani@pawsey.org.au>
2024-10-31 22:25:54 +0800

    vcontrol for maintenance

---
+++ vscheduler/modules/guaca/maintenance.py
@@ -0,0 +1,41 @@
+# removes/adds users to general pool at maintenance
+import guacamole
+import pandas as pd
+from vscheduler.log.log import Capture_log
+from vscheduler.lib.config import Credentials as MyCredentials
+from vscheduler.general.initiate import PrintCondition as MyPrintCondition
+
+records = Capture_log("maintenance", __file__)
+logger_win = records.log_agent("windows")
+logger_unix = records.log_agent("linux")
+
+session = guacamole.session("https://dev-guacamole.pawsey.org.au", "mysql", "guacadmin", "REMOVED")
+
+def change_maint_status (mode, partition):
+    try:
+        users = session.list_users()
+        general_pool_users = []
+        [general_pool_users.append(user) for user in users if partition in session.detail_user_groups(user)]
+        general_pool_users_df = pd.DataFrame({'USERNAME':general_pool_users})
+        general_pool_users_df[partition] = "*"
+        print (f"\n{partition} users: ({len(general_pool_users_df)})\n{general_pool_users_df.sort_values('USERNAME', ascending=True)}") if MyPrintCondition.fprint else 0
+        logger_unix.info (f"\n{partition} users: ({len(general_pool_users_df)})\n{general_pool_users_df.sort_values('USERNAME', ascending=True)}") if partition == MyCredentials.linux_pool else 0
+        logger_win.info (f"\n{partition} users: ({len(general_pool_users_df)})\n{general_pool_users_df.sort_values('USERNAME', ascending=True)}") if partition == MyCredentials.windows_pool else 0
+    except session.error as e:
+        print (f"error in checking guacamole users:\n{e}") if MyPrintCondition.fprint else 0
+        logger_unix.info (f"error in checking guacamole users:\n{e}") if partition == MyCredentials.linux_pool else 0
+        logger_win.info (f"error in checking guacamole users:\n{e}") if partition == MyCredentials.windows_pool else 0
+    if mode == "create":
+        [(session.update_user_group(user, partition, "remove") for user in general_pool_users_df['USERNAME'].values.tolist())]
+        print (f"Maintenance mode successfully enabled for < {partition} >\nusers access to < {partition} > was removed.") if MyPrintCondition.fprint else 0
+        logger_unix.info (f"Maintenance mode successfully enabled for < {partition} >\nusers access to < {partition} > was removed.") if partition == MyCredentials.linux_pool else 0
+        logger_win.info (f"Maintenance mode successfully enabled for < {partition} >\nusers access to < {partition} > was removed.") if partition == MyCredentials.windows_pool else 0
+        # also, need to remove users from allocated nodes
+        # ****** change the parttion availability, so vinfo shows that as down <--- For now, manual change in config till design the best solution
+    elif mode == "delete":
+        [(session.update_user_group(user, partition, "add") for user in general_pool_users_df['USERNAME'].values.tolist())]
+        print (f"Maintenance mode successfully disabled for < {partition} >\nusers access to < {partition} > was enabled.") if MyPrintCondition.fprint else 0
+        logger_unix (f"Maintenance mode successfully disabled for < {partition} >\nusers access to < {partition} > was enabled.") if partition == MyCredentials.linux_pool else 0
+        logger_win (f"Maintenance mode successfully disabled for < {partition} >\nusers access to < {partition} > was enabled.") if partition == MyCredentials.windows_pool else 0
+        # no need to put users back to their allocated nodes before maintenance
+        # ****** change the parttion availability, so vinfo shows that as up <--- For now, manual change in config till design the best solution   
---
+++ vscheduler/tools/control.py
@@ -0,0 +1,94 @@
+# makes a node exception not to be called in allocation process, or gives exception status of all/specific node(s)
+import multiprocessing, click
+from vscheduler.log.log import Capture_log
+from vscheduler.lib.config import Credentials as MyCredentials
+from vscheduler.general.timer import Brackets as MyBrackets
+from vscheduler.general.initiate import Initiation as initiate
+from vscheduler.general.initiate import PrintCondition as MyPrintCondition
+# from vscheduler.modules.cluster.maintenance import activation as activate
+# from vscheduler.modules.cluster.maintenance import deactivation as deactivate
+# from vscheduler.modules.cluster.maintenance import activation_status as status_activation
+from vscheduler.modules.guaca.maintenance import change_maint_status
+
+records = Capture_log("exception", __file__)
+logger = records.log_agent()
+
+# Process class
+# class Process(multiprocessing.Process):
+#     def __init__(self, id, status, node):
+#         super(Process, self).__init__()
+#         self.id = id
+#         self.hostname = node
+#         self.status = status
+    
+#     def run(self):
+#         if self.status == "activate":
+#             activate (self.hostname)
+#         elif self.status == "deactivate":    
+#             deactivate (self.hostname)
+#         elif self.status == "status":
+#             start = '2000-01-01' if not initiate.start else initiate.start
+#             end = MyBrackets.local_time if not initiate.end else initiate.end
+#             print (f"start: {start}")
+#             print (f"end: {end}")
+#             status_activation (self.hostname, start, end)
+
+def main():
+    if not initiate.node:
+        change_maint_status (initiate.mode, initiate.partition)
+        # for i in range (MyCredentials.windows_general_range[0], MyCredentials.windows_general_range[1]+1):
+        #     node = MyCredentials.windows_node_name + '0' + str(i) if i <= 9 else MyCredentials.windows_node_name + str(i)
+        #     if initiate.status == 'activate':
+        #         p = Process(i, "activate", node)
+        #     elif initiate.status == 'deactivate':
+        #         p = Process(i, "deactivate", node)
+        #     elif initiate.status == 'status':
+        #         p = Process(i, "status", node)
+        #     p.start()       # Create a new process and invoke the Process.run() method
+        #     p.join()        # Process.join() to wait for task completion
+        # for i in range (MyCredentials.windows_booking_range[0], MyCredentials.windows_booking_range[1]+1):
+        #     node = MyCredentials.windows_node_name + '0' + str(i) if i <= 9 else MyCredentials.windows_node_name + str(i)
+        #     if initiate.status == 'activate':
+        #         p = Process(i, "activate", node)
+        #     elif initiate.status == 'deactivate':
+        #         p = Process(i, "deactivate", node)
+        #     elif initiate.status == 'status':
+        #         p = Process(i, "status", node)
+        #     p.start()       # Create a new process and invoke the Process.run() method
+        #     p.join()        # Process.join() to wait for task completion
+        # for i in range (MyCredentials.linux_general_range[0], MyCredentials.linux_general_range[1]+1):
+        #     node = MyCredentials.linux_node_name + '0' + str(i) if i <= 9 else MyCredentials.linux_node_name + str(i)
+        #     if initiate.status == 'activate':
+        #         p = Process(i, "activate", node)
+        #     elif initiate.status == 'deactivate':
+        #         p = Process(i, "deactivate", node)
+        #     elif initiate.status == 'status':
+        #         p = Process(i, "status", node)
+        #     p.start()       # Create a new process and invoke the Process.run() method
+        #     p.join()        # Process.join() to wait for task completion
+        # for i in range (MyCredentials.linux_booking_range[0], MyCredentials.linux_booking_range[1]+1):
+        #     node = MyCredentials.linux_node_name + '0' + str(i) if i <= 9 else MyCredentials.linux_node_name + str(i)
+        #     if initiate.status == 'activate':
+        #         p = Process(i, "activate", node)
+        #     elif initiate.status == 'deactivate':
+        #         p = Process(i, "deactivate", node)
+        #     elif initiate.status == 'status':
+        #         p = Process(i, "status", node)
+        #     p.start()       # Create a new process and invoke the Process.run() method
+        #     p.join()        # Process.join() to wait for task completion
+    else:
+        # if initiate.add:
+        #     p = Process("", "add", initiate.node)
+        # elif initiate.remove:
+        #     p = Process("", "remove", initiate.node)
+        # elif initiate.status:
+        #     p = Process("", "status", initiate.node)
+        # p.start()       # Create a new process and invoke the Process.run() method
+        # p.join()        # Process.join() to wait for task completion
+        print ("Maintenance mode will be applied for all nodes; To exclude specific node, use vexcept")
+    # p = Process("", initiate.status, initiate.node)
+    # p.start()       # Create a new process and invoke the Process.run() method
+    # p.join()        # Process.join() to wait for task completion
+    
+if __name__ == '__main__':
+    main()
