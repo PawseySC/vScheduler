8438b09a7af42bdee90064220550a027c4b7098b
Ali Zamani <ali.zamani@pawsey.org.au>
2025-01-02 11:18:14 +0800

    vscheduler/log folder tidy

--- vscheduler/log/log.py
+++ vscheduler/log/log.py
@@ -1,17 +1,16 @@
 import logging
-from vscheduler.lib.config import Credentials as MyCredentials
+from vscheduler.lib.config import Credentials
 
 class Capture_log(object):
     """
-    Logging class to call in methods capture event 
-    logs into dedicated log file for booking and pool.
+    Logging class to be called in methods capturing event logs into dedicated log file
     """
     def __init__(self, flag, location):
         self.formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s %(message)s')
         self.flag = flag
         self.location = location
     
-    def extendable_logger(self, log_name, file_name, level=MyCredentials.log_level):
+    def extendable_logger(self, log_name, file_name, level=Credentials.log_level):
         handler = logging.FileHandler(file_name)
         handler.setFormatter(self.formatter) 
         specified_logger = logging.getLogger(log_name)
