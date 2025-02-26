e8d985b6cf005fcda0b1a90f6a657a8cba90b7e3
Ali Zamani <ali.zamani@pawsey.org.au>
2025-02-26 17:26:05 +0800

    log rotate added

--- vscheduler/log/log.py
+++ vscheduler/log/log.py
@@ -1,7 +1,8 @@
 import os, logging
+import logging.handlers as handlers
 from vscheduler.lib.config import Config
-print (Config)
-print (type(Config))
+
+
 class CaptureLog(object):
     """
     Logging class facilitates methods to capture event logs into dedicated log file
@@ -12,7 +13,8 @@ class CaptureLog(object):
         self.location = location
     
     def extendable_logger(self, log_name, file_name, level = Config.config["log"]['level']):
-        handler = logging.FileHandler(file_name)
+        # handler = logging.FileHandler(file_name)                                                              # contineous logging in same file
+        handler = handlers.TimedRotatingFileHandler(file_name, when="d", interval=1)    # , backupCount=30      # logs into separate files for configured time intervals
         handler.setFormatter(self.formatter) 
         specified_logger = logging.getLogger(log_name)
         specified_logger.setLevel(level)
@@ -20,6 +22,6 @@ class CaptureLog(object):
         return specified_logger
     
     def log_agent(self, partition):
-        log_file = os.path.join(os.path.dirname(__file__), '.', f'{partition}.log')
+        log_file = os.path.join(os.path.dirname(__file__), f'{partition}.log')
         agent_logger = self.extendable_logger(f"{self.flag} {self.location}", f'{log_file}')
         return agent_logger
