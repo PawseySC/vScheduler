b78cc31b6a0ac9c52abea13f5d842b932118f780
Ali Zamani <ali.zamani@pawsey.org.au>
2025-07-11 05:14:05 -0400

    removed click

--- vscheduler/tools/quota.py
+++ vscheduler/tools/quota.py
@@ -1,4 +1,4 @@
-import multiprocessing, click
+import multiprocessing
 from vscheduler.log.log import CaptureLog
 from vscheduler.lib.config import Config
 from vscheduler.general.initiate import Initiation as initiate
