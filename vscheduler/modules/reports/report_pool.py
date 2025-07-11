03eec7c7ddf2b565a79d2aec4645ac1ceaa4d3f3
Ali Zamani <ali.zamani@pawsey.org.au>
2025-07-11 05:18:57 -0400

    tweaked single and double quotes

--- vscheduler/modules/reports/report_pool.py
+++ vscheduler/modules/reports/report_pool.py
@@ -61,7 +61,7 @@ def pool_report_generator(hostname, username, start, end):
                         query = query + f"end <= '{end}'"
                         
     # print (query)
-    actual_usage_report = f"SELECT * FROM {config.get("database.report.table.linux")}" + query
+    actual_usage_report = f"SELECT * FROM {config.get('database.report.table.linux')}" + query
     actual_usage_report_results = pd.read_sql(actual_usage_report, my_connection)
     actual_usage_report_results["email"] = ""
     actual_usage_report_results["first name"] = ""
