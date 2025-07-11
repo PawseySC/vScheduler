bbc82e8a0f43a92f278a4fcd4a0d7ea3b28d0b76
Ali Zamani <ali.zamani@pawsey.org.au>
2025-07-11 07:03:12 -0400

    vinfo needs to be redesigned - working for now

--- vscheduler/tools/info.py
+++ vscheduler/tools/info.py
@@ -26,45 +26,45 @@ def main():
     """
     vinfo script giving overview of all nodes' status
     """
-    if initiate.session:
-        print (initiate.session)
-        if not initiate.node:
-            if config.get("partition.windows.booking.status") or config.get("partition.windows.general.status"):
-                if config.get("partition.windows.booking.status"):
-                    for i in range (config.get("partition.windows.booking.range")[0], config.get("partition.windows.booking.range")[1]+1):
-                        node = config.get("partition.windows.node") + '0' + str(i) if i <= 9 else config.get("partition.windows.node") + str(i)
-                        p = Process(i, node)
-                        p.start()       # Create a new process and invoke the Process.run() method
-                        p.join()        # Process.join() to wait for task completion
-                if config.get("partition.windows.general.status"):
-                    for i in range (config.get("partition.windows.general.range")[0], config.get("partition.windows.general.range")[1]+1):
-                        node = config.get("partition.windows.node") + '0' + str(i) if i <= 9 else config.get("partition.windows.node") + str(i)
-                        p = Process(i, node)
-                        p.start()       # Create a new process and invoke the Process.run() method
-                        p.join()        # Process.join() to wait for task completion
-            else:
-                print ("There is no Windows partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
-                logger.info ("There is no Windows partition; To enable it edit vscheduler confilg")
-            if config.get("partition.linux.booking.status") or config.get("partition.linux.general.status"):
-                if config.get("partition.linux.booking.status"):
-                    for i in range (config.get("partition.linux.booking.range")[0], config.get("partition.linux.booking.range")[1]+1):
-                        node = config.get("partition.linux.node") + '0' + str(i) if i <= 9 else config.get("partition.linux.node") + str(i)
-                        p = Process(i, node)
-                        p.start()       # Create a new process and invoke the Process.run() method
-                        p.join()        # Process.join() to wait for task completion
-                if config.get("partition.linux.general.status"):
-                    for i in range (config.get("partition.linux.general.range")[0], config.get("partition.linux.general.range")[1]+1):
-                        node = config.get("partition.linux.node") + '0' + str(i) if i <= 9 else config.get("partition.linux.node") + str(i)
-                        p = Process(i, node)
-                        p.start()       # Create a new process and invoke the Process.run() method
-                        p.join()        # Process.join() to wait for task completion
-            else:
-                print ("There is no Linux partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
-                logger.info ("There is no Linux partition; To enable it edit vscheduler confilg")    
-        elif initiate.node:
-            p = Process("", initiate.node)
-            p.start()       # Create a new process and invoke the Process.run() method
-            p.join()        # Process.join() to wait for task completion
+    # if initiate.session:
+    #     print (initiate.session)
+    # if not initiate.node:
+    #     if config.get("partition.windows.booking.status") or config.get("partition.windows.general.status"):
+    #         if config.get("partition.windows.booking.status"):
+    #             for i in range (config.get("partition.windows.booking.range")[0], config.get("partition.windows.booking.range")[1]+1):
+    #                 node = config.get("partition.windows.node") + '0' + str(i) if i <= 9 else config.get("partition.windows.node") + str(i)
+    #                 p = Process(i, node)
+    #                 p.start()       # Create a new process and invoke the Process.run() method
+    #                 p.join()        # Process.join() to wait for task completion
+    #         if config.get("partition.windows.general.status"):
+    #             for i in range (config.get("partition.windows.general.range")[0], config.get("partition.windows.general.range")[1]+1):
+    #                 node = config.get("partition.windows.node") + '0' + str(i) if i <= 9 else config.get("partition.windows.node") + str(i)
+    #                 p = Process(i, node)
+    #                 p.start()       # Create a new process and invoke the Process.run() method
+    #                 p.join()        # Process.join() to wait for task completion
+    #     else:
+    #         print ("There is no Windows partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
+    #         logger.info ("There is no Windows partition; To enable it edit vscheduler confilg")
+    #     if config.get("partition.linux.booking.status") or config.get("partition.linux.general.status"):
+    #         if config.get("partition.linux.booking.status"):
+    #             for i in range (config.get("partition.linux.booking.range")[0], config.get("partition.linux.booking.range")[1]+1):
+    #                 node = config.get("partition.linux.node") + '0' + str(i) if i <= 9 else config.get("partition.linux.node") + str(i)
+    #                 p = Process(i, node)
+    #                 p.start()       # Create a new process and invoke the Process.run() method
+    #                 p.join()        # Process.join() to wait for task completion
+    #         if config.get("partition.linux.general.status"):
+    #             for i in range (config.get("partition.linux.general.range")[0], config.get("partition.linux.general.range")[1]+1):
+    #                 node = config.get("partition.linux.node") + '0' + str(i) if i <= 9 else config.get("partition.linux.node") + str(i)
+    #                 p = Process(i, node)
+    #                 p.start()       # Create a new process and invoke the Process.run() method
+    #                 p.join()        # Process.join() to wait for task completion
+    #     else:
+    #         print ("There is no Linux partition; To enable it edit vscheduler confilg") if MyPrintCondition.fprint else 0
+    #         logger.info ("There is no Linux partition; To enable it edit vscheduler confilg")    
+    if initiate.node:
+        p = Process("", initiate.node)
+        p.start()       # Create a new process and invoke the Process.run() method
+        p.join()        # Process.join() to wait for task completion
     else:
         print_info()
 
